b8420f08977d398dde145155f3621c21f361bb49
Anuj Panchal <anujpan1@gmail.com>
2019-01-21 23:44:36 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -131,16 +131,3 @@ SQL
     end
   end
 end
-© 2019 GitHub, Inc.
-Terms
-Privacy
-Security
-Status
-Help
-Contact GitHub
-Pricing
-API
-Training
-Blog
-About
-Press h to open a hovercard with more details.
